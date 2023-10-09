05f484647cebfd6c189f1dc65d1acf3b1ea1f568
Abia <111609608+abiagreat@users.noreply.github.com>
2023-10-08 20:25:12 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,42 +1,75 @@
 #include <iostream>
 #include <fstream>
-#include <stack>
-#include <string>
+#include <stdexcept>
+
+template <typename T>
+class DynamicStack {
+private:
+    struct Node {
+        T data;
+        Node* next;
+    };
+
+    Node* top;
+
+public:
+    DynamicStack() : top(nullptr) {}
+
+    ~DynamicStack() {
+        while (!isEmpty()) {
+            pop();
+        }
+    }
+
+    void push(const T& item) {
+        Node* newNode = new Node{item, top};
+        top = newNode;
+    }
+
+    void pop() {
+        if (isEmpty()) {
+            throw std::runtime_error("Stack is empty");
+        }
+        Node* temp = top;
+        top = top->next;
+        delete temp;
+    }
+
+    bool isEmpty() const {
+        return top == nullptr;
+    }
+};
 
 int main() {
+    DynamicStack<char> charStack;
+
     std::ifstream inputFile("input.txt");
     std::ofstream outputFile("output.txt");
 
-    if (!inputFile.is_open()) {
-        std::cerr << "Error: Cannot open input file." << std::endl;
+    if (!inputFile) {
+        std::cerr << "Error opening input file." << std::endl;
         return 1;
     }
 
-    if (!outputFile.is_open()) {
-        std::cerr << "Error: Cannot open output file." << std::endl;
+    if (!outputFile) {
+        std::cerr << "Error opening output file." << std::endl;
         return 1;
     }
 
-    std::stack<char> charStack;
     char ch;
-
-    // Read characters from the input file and push them onto the stack
     while (inputFile.get(ch)) {
         charStack.push(ch);
     }
 
-    // Pop characters from the stack and write them to the output file
-    while (!charStack.empty()) {
-        ch = charStack.top();
+    while (!charStack.isEmpty()) {
+        outputFile.put(charStack.top->data);
         charStack.pop();
-        outputFile << ch;
     }
 
-    std::cout << "File reversed and saved successfully." << std::endl;
-
-    // Close the file streams
     inputFile.close();
     outputFile.close();
+
+    std::cout << "File reversed successfully!" << std::endl;
 
     return 0;
 }
