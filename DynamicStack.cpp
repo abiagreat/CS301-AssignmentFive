b00e3fefbf078c0d70cf0ca2b47bbc5636ffa606
marioEastBay <144731067+marioEastBay@users.noreply.github.com>
2023-10-12 16:57:48 -0700

    Update DynamicStack.cpp

---
+++ DynamicStack.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+#include "DynamicStack.h"
+
+template <typename T>
+DynamicStack<T>::DynamicStack() {
+    top = nullptr;
+}
+
+template <typename T>
+DynamicStack<T>::~DynamicStack() {
+    while (top != nullptr) {
+        pop();
+    }
+}
+
+template <typename T>
+void DynamicStack<T>::push(T item) {
+    Node* location;
+    location = new Node;
+    location->data = item;
+    location->next = top;
+    top = location;
+}
+
+template <typename T>
+void DynamicStack<T>::pop() {
+    Node* temp = top;
+    top = top->next;
+    delete temp;
+}
+
+template <typename T>
+bool DynamicStack<T>::isEmpty() const {
+    return top == nullptr;
+}
+
+template <typename T>
+bool DynamicStack<T>::isFull() const {
+    try {
+        Node* location = new Node;
+        delete location;
+        return false;
+    } catch (bad_alloc exception) {
+        return true;
+    }
+}
+
+template <typename T>
+T DynamicStack<T>::peek() const {
+    if (isEmpty()) {
+        throw std::runtime_error("Stack is empty");
+    }
+    return top->data;
+}
